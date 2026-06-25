8aafff16dab9ff90c1c8e75dd33b7b4e547b4c68
xcia4899 <xcia48996531@gmail.com>
2026-06-25 16:58:22 +0800

    hero RWD 製作中

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,10 @@ import { scrollAnimation } from "./ulite/scrollAnimation.js";
 import { scrollHeader } from "./ulite/scrollHeader.js";
 import { goTop } from "./ulite/goTop.js";
 import { initHeader } from "./header.js";
+import { loadSvgSprite } from "./ulite/svgSprite.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
+    await loadSvgSprite();
     await loadLayout();
     initHeader();
     initMenu();
